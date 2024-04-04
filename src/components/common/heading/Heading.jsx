dd1052ed24df8e10ca6aabb57d2aa94226748af5
Henriette <henriettedoloo@gmail.com>
2024-04-04 10:44:34 +0300

    Changed styling for nav-links

--- src/components/common/heading/Heading.jsx
+++ src/components/common/heading/Heading.jsx
@@ -5,17 +5,25 @@ import { Link } from 'react-router-dom/cjs/react-router-dom.min'
 
 const Heading = () => {
     const[click, setClick] = useState(false)
+
+    const [clickedLink, setClickedLink] = useState(null);
+
+    const handleLinkClick = (linkName) => {
+        setClickedLink(linkName);
+    };
+
+
   return (
     <>
      <Head/> 
      <header>
         <nav className='flexSB'>
             <ul className={click ? "mobile-nav":'flexSB'} onClick={()=> setClick(false)}>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/about">About</Link></li>
-                <li><Link to="/skills">Skills</Link></li>
-                <li><Link to="/projects">Projects</Link></li>
-                <li><Link to="/contact">Contact</Link></li>
+                <li><Link to="/" className={clickedLink === "/" ? "clicked" : ""} onClick={() => handleLinkClick("/")}>Home</Link></li>
+                <li><Link to="/about" className={clickedLink === "about" ? "clicked" : ""} onClick={() => handleLinkClick("about")}>About</Link></li>
+                <li><Link to="/skills" className={clickedLink === "skills" ? "clicked" : ""} onClick={() => handleLinkClick("skills")}>Skills</Link></li>
+                <li><Link to="/projects" className={clickedLink === "projects" ? "clicked" : ""} onClick={() => handleLinkClick("projects")}>Projects</Link></li>
+                <li><Link to="/contact" className={clickedLink === "contact" ? "clicked" : ""} onClick={() => handleLinkClick("contact")}>Contact</Link></li>
 
             </ul>
             <div className="start">
